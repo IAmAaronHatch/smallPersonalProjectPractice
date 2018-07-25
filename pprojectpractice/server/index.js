ea514a5c3423fc1ce6500f8b974a1ec50d117027
Aaron Hatch <aaron@Aarons-MacBook-Pro.local>
2018-07-25 14:53:43 -0600

    start

---
+++ pprojectpractice/server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+
+
+
+
+
+
+
+const PORT = 4200
+app.listen(PORT, () => console.log('I AM WATCHING', PORT))
